78a4bd6083aa1968dd5a084fc289930515b7e276
Tetiana Reshetnik <reshetniktati@gmail.com>
2019-11-11 22:55:26 +0200

    delete commits

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,14 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-// import EditUserInfo from '../views'
-// import EditPreferences from '../components/SettingsPage/SettingsPageTabs/EditPreferences.vue'
-// import EditFollowers from '../components/SettingsPage/SettingsPageTabs/EditFollowers.vue'
-// import EditLiked from '../components/SettingsPage/SettingsPageTabs/EditLiked.vue'
-// import EditPassword from '../components/SettingsPage/SettingsPageTabs/EditPassword.vue'
-// import EditPrivacy from '../components/SettingsPage/SettingsPageTabs/EditPrivacy.vue'
-// import EditSaved from '../components/SettingsPage/SettingsPageTabs/EditSaved.vue'
-// import DeleteAccount from '../components/SettingsPage/SettingsPageTabs/DeleteAccount.vue'
 
 Vue.use(VueRouter)
 
