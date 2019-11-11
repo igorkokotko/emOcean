eb4f0b417db04003cffebf657a158817b94dabb0
Tetiana Reshetnik <reshetniktati@gmail.com>
2019-11-11 22:46:52 +0200

    dynamic-build menu

--- client/src/views/Settings/SettingsMenu.vue
+++ client/src/views/Settings/SettingsMenu.vue
@@ -1,32 +1,39 @@
 <template>
   <ul>
-    <li><router-link to='/settings/editProfile'>User profile</router-link></li>
-    <li><router-link to='/settings/editPreferences'>Preferences</router-link></li>
-    <li><router-link to='/settings/editFollowers'>Followers</router-link></li>
-    <li><router-link to='/settings/editSaved'>Saved</router-link></li>
-    <li><router-link to='/settings/editLiked'>Liked</router-link></li>
-    <li><router-link to='/settings/editPrivacy'>Privacy</router-link></li>
-    <li><router-link to='/settings/changePassword'>Change password</router-link></li>
-    <li><router-link to='/settings/deleteAccount'>Delete account</router-link></li>
+    <li v-for="(item, index) in routeItems" :key='index'>
+      <router-link :to="item.to">{{item.title}}</router-link>
+    </li>
   </ul>
 </template>
 
 <script>
 export default {
-  name: 'settings-menu'
+  name: 'settings-menu',
+  data: function () {
+    return {
+      routeItems: [
+        { to: '/settings/editProfile', title: 'User profile', show: true },
+        { to: '/settings/editPreferences', title: 'Preferences', show: true },
+        { to: '/settings/editFollowers', title: 'Followers', show: true },
+        { to: '/settings/editSaved', title: 'Saved', show: true },
+        { to: '/settings/editLiked', title: 'Liked', show: true },
+        { to: '/settings/editPrivacy', title: 'Privacy', show: true },
+        { to: '/settings/changePassword', title: 'Change password', show: true },
+        { to: '/settings/deleteAccount', title: 'Delete account', show: true }
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
+ul {
+  list-style: none;
+  padding-inline-start: 0;
+  width: 200px;
+}
 
-  ul {
-    list-style: none;
-    padding-inline-start: 0;
-    width: 200px;
-  }
-
-  li {
-    border: 1px solid;
-  }
-
+li {
+  border: 1px solid;
+}
 </style>
