3111f811513290b718ec75b95284e54565306924
nimchuk <nimchuk2011@gmail.com>
2019-11-12 16:36:56 +0200

    Update createToken.js
    Fixed name of a jwt value

--- server/common/createToken.js
+++ server/common/createToken.js
@@ -1,7 +1,8 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 module.exports = createJwtToken = value => {
-  return jwt.sign({ id: value }, process.env.JWT_SECRET, {
+  return jwt.sign({ value }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE
-  })
-}
+    // httpOnly: true
+  });
+};
