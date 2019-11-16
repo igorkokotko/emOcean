3f8dccef4384d55e60fb61a37a238e960919b12f
nimchuk <nimchuk2011@gmail.com>
2019-11-17 00:36:44 +0200

    Remove unnecessary coment
    Remove unnecessary coment

--- server/middleware/asyncMiddleware.js
+++ server/middleware/asyncMiddleware.js
@@ -1,5 +1,3 @@
-// https://medium.com/@Abazhenov/using-async-await-in-express-with-node-8-b8af872c0016
-// stolen from this topic, nothing special, but works great
 const asyncMiddleware = fn => (req, res, next) => {
   Promise.resolve(fn(req, res, next)).catch(next)
 }
