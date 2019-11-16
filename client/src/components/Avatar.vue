3ca82ce25df0071b562d65bff1b1f028272f3035
Zahakailo <o.zahakailo@gmail.com>
2019-11-16 22:34:00 +0200

    Avatar component base

---
+++ client/src/components/Avatar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <q-avatar
+      size="100px"
+      font-size="size"
+      color="primary"
+      text-color="white"
+      :icon="img.length === 0 ? 'account_circle' : null"
+    >
+      <img v-if="img.length > 0" :src="img">
+    </q-avatar>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    img: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
