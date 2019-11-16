1dea829741c21d473b99ba6ca32dd15affe1ddfb
nimchuk <nimchuk2011@gmail.com>
2019-11-17 00:37:37 +0200

    Error messages from db
    Remove unnecessary coment, and changed  a bit some error messages from db

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -1,12 +1,24 @@
-// TODO Check which errors can be thrown from db or other directions and create
-// own message
-const CustomError = require("../common/CustomError");
+const CustomError = require('../common/CustomError')
 
 const errorHandler = (err, req, res, next) => {
-  let error = { ...err };
+  const error = { ...err }
 
-  error.message = err.message;
-  res.status(400).json({ error: error.message });
-};
+  error.message = err.message
+  if (
+    error.message ===
+    'There is no user record corresponding to this identifier. The user may have been deleted.'
+  ) {
+    error.message = 'User with entered email not found.'
+  }
+  if (
+    error.message ===
+    'The password is invalid or the user does not have a password.'
+  ) {
+    error.message = 'You entered a wrong password. Please try again.'
+  }
+  res
+    .status(err.status || 400)
+    .json({ error: error.message || 'Unexpected error' })
+}
 
-module.exports = errorHandler;
+module.exports = errorHandler
