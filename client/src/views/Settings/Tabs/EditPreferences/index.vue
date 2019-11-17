90e3ef5471f1ac5aa1577501a179a39c49bcb2cf
Tetiana Reshetnik <reshetniktati@gmail.com>
2019-11-18 00:28:49 +0200

    added pictures

--- client/src/views/Settings/Tabs/EditPreferences/index.vue
+++ client/src/views/Settings/Tabs/EditPreferences/index.vue
@@ -1,9 +1,37 @@
 <template>
-  <p>Preferences</p>
+  <div>
+    <q-btn
+      round
+      v-for="(preference, index) in preferences" :key="index">
+      <q-avatar size="42px">
+        <img :src="preference.src">
+      </q-avatar>
+    </q-btn>
+    <img src="@/assets/preferences/animals.jpg">
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'EditPreferences'
+  name: 'EditPreferences',
+  data () {
+    return {
+      preferences: [
+        { src: '@/assets/preferences/animals.jpg', title: 'animals' }
+        // {src='@/assets/preferences/cinema.jpg', title='cinema'},
+        // {src='@/assets/preferences/culinary.jpg', title='culinary'},
+        // {src='@/assets/preferences/dance.jpg', title='dance'},
+        // {src='@/assets/preferences/extreme.jpg', title='extreme'},
+        // {src='@/assets/preferences/game.jpg', title='game'},
+        // {src='@/assets/preferences/health.jpg', title='health'},
+        // {src='@/assets/preferences/learn.jpg', title='learn'},
+        // {src='@/assets/preferences/nature.jpg', title='nature'},
+        // {src='@/assets/preferences/prank.jpg', title='prank'},
+        // {src='@/assets/preferences/science.jpg', title='science'},
+        // {src='@/assets/preferences/sport.jpg', title='sport'},
+        // {src='@/assets/preferences/travel.jpg', title='travel'}
+      ]
+    }
+  }
 }
 </script>
