83f5e9ad96249ef33c6b9c57f0c62c4b28c78948
Orest Kozolup <kozolup.yakiv@gmail.com>
2019-11-19 22:59:24 +0100

    added getters

--- client/src/store/modules/authentication.js
+++ client/src/store/modules/authentication.js
@@ -4,16 +4,24 @@ const state = {
   notifyRegistered: false,
   notifyReset: false
 }
+const getters = {
+  notifyRegistered: state => {
+    return state.notifyRegistered
+  },
+  notifyReset: state => {
+    return state.notifyReset
+  }
+}
 const mutations = {
   login (state, payload) {
     state.token = payload.token
     state.user = payload.user
   },
-  notifyRegistered (state, n) {
-    state.notifyRegistered = n
+  notifyRegistered (state, boolean) {
+    state.notifyRegistered = boolean
   },
-  notifyReset (state, n) {
-    state.notifyReset = n
+  notifyReset (state, boolean) {
+    state.notifyReset = boolean
   }
 }
 const actions = {
@@ -21,6 +29,7 @@ const actions = {
 
 export default {
   state,
+  getters,
   mutations,
   actions
 }
