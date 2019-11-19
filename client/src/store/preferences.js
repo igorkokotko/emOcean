160a99a4a40d83bb51f1d40c1f95e64111140eb3
Tetiana Reshetnik <reshetniktati@gmail.com>
2019-11-19 22:20:02 +0200

    fied state, made img captions

--- client/src/store/preferences.js
+++ client/src/store/preferences.js
@@ -7,62 +7,62 @@ const state = {
     },
     'id_2': {
       src: '/preferences/cinema.jpg',
-      title: 'animals',
+      title: 'cinema',
       chosen: false
     },
     'id_3': {
       src: '/preferences/culinary.jpg',
-      title: 'animals',
+      title: 'culinary',
       chosen: false
     },
     'id_4': {
       src: '/preferences/dance.jpg',
-      title: 'animals',
+      title: 'dance',
       chosen: false
     },
     'id_5': {
       src: '/preferences/extreme.jpg',
-      title: 'animals',
+      title: 'extreme',
       chosen: false
     },
     'id_6': {
       src: '/preferences/game.jpg',
-      title: 'animals',
+      title: 'game',
       chosen: false
     },
     'id_7': {
       src: '/preferences/health.jpg',
-      title: 'animals',
+      title: 'health',
       chosen: false
     },
     'id_8': {
       src: '/preferences/learn.jpg',
-      title: 'animals',
+      title: 'learn',
       chosen: false
     },
     'id_9': {
       src: '/preferences/nature.jpg',
-      title: 'animals',
+      title: 'nature',
       chosen: false
     },
     'id_10': {
       src: '/preferences/prank.jpg',
-      title: 'animals',
+      title: 'prank',
       chosen: false
     },
     'id_11': {
       src: '/preferences/science.jpg',
-      title: 'animals',
+      title: 'science',
       chosen: false
     },
     'id_12': {
       src: '/preferences/sport.jpg',
-      title: 'animals',
+      title: 'sport',
       chosen: false
     },
     'id_13': {
       src: '/preferences/travel.jpg',
-      title: 'animals',
+      title: 'travel',
       chosen: false
     }
   }
--- client/src/views/Settings/Tabs/EditPreferences/index.vue
+++ client/src/views/Settings/Tabs/EditPreferences/index.vue
@@ -8,9 +8,23 @@
       round
       class="q-ma-xs"
     >
-      <q-avatar size="150px" >
-        <img :src="preference.src" :class ="{chosen: preference.chosen}">
+      <q-avatar size="150px">
+        <img :src="preference.src" :class ="{chosen: preference.chosen}"/>
+          <div v-bind:class="[preference.chosen ? 'chosen' : 'img-caption',
+            'absolute-center',
+            'text-h6',
+            'text-center']"
+          >
+          {{preference.title}}
+          </div>
       </q-avatar>
+    </q-btn>
+    <q-btn
+      size="50px"
+      round
+      color="teal"
+      icon="video_library"
+    >
     </q-btn>
   </div>
 </template>
@@ -30,8 +44,26 @@ export default {
 </script>
 
 <style scoped>
-.chosen{
-  border: 10px solid  rgba(90, 251, 224, 0.171);
-  filter: blur(0px);
+.chosen {
+  background-color: rgba(2, 211, 176, 0.801);
+  color: #000;
+  width: 90%;
+  height: 20%;
+  align-self: center;
+  border-radius: 0 40px 0 40px;
+  }
+img.chosen {
+  filter: grayscale(100%);
+  /* filter: blur(2px); */
 }
+
+.img-caption {
+  background-color: rgba(39, 39, 39, 0.602);
+  color: #fff;
+  width: 90%;
+  height: 20%;
+  align-self: center;
+  border-radius: 0 40px 0 40px
+}
+
 </style>
