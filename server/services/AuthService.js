83253b6fed1fe432d7c713cd33c06097d6cd5138
nimchuk <nimchuk2011@gmail.com>
2019-11-20 03:48:11 +0200

    Added the logic of checking the nickname for uniqueness

--- server/services/AuthService.js
+++ server/services/AuthService.js
@@ -1,72 +1,89 @@
-const { db, auth } = require('../config/databaseConfig')
+const { db, auth } = require("../config/databaseConfig");
+const CustomError = require("../common/CustomError");
 
 const createUserWithEmailAndPassword = (email, password, nickname) => {
   return new Promise((resolve, reject) => {
-    auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then(({ user }) => {
-        db.collection('users')
-          .doc(user.uid)
-          .set({ nickname, profileId: user.uid })
-          .then(res => resolve('User created'))
-          .catch(err => reject(err))
-      })
-      .catch(err => reject(err))
-  })
-}
+    const usersRef = db.collection("users");
+    usersRef
+      .where("nickname", "==", nickname)
+      .get()
+      .then(snapshot => {
+        if (snapshot.empty) {
+          auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then(({ user }) => {
+              usersRef
+                .doc(user.uid)
+                .set({ nickname, profileId: user.uid })
+                .then(res => resolve("User created"))
+                .catch(err => reject(err));
+            })
+            .catch(err => reject(err));
+        } else {
+          reject(
+            new CustomError({
+              name: "DatabaseError",
+              message: "Nickname already taken. Try with something else",
+              status: 400
+            })
+          );
+        }
+      });
+  });
+};
 const loginWithEmailAndPassword = function(email, password) {
   return new Promise(function(resolve, reject) {
     auth()
       .signInWithEmailAndPassword(email, password)
       .then(({ user }) => resolve(user))
-      .catch(err => reject(err))
-  })
-}
+      .catch(err => reject(err));
+  });
+};
 
 const changePassword = function(oldPassword, newPassword, email) {
   return new Promise(function(resolve, reject) {
-    const credential = auth.EmailAuthProvider.credential(email, oldPassword)
+    const credential = auth.EmailAuthProvider.credential(email, oldPassword);
     auth()
       .signInWithCredential(credential)
       .then(() => {
         auth()
           .currentUser.updatePassword(newPassword)
-          .then(() => resolve('Password changed'))
-          .catch(err => reject(err))
+          .then(() => resolve("Password changed"))
+          .catch(err => reject(err));
       })
-      .catch(err => reject(err))
-  })
-}
+      .catch(err => reject(err));
+  });
+};
 
 const sendPasswordResetCode = function(email) {
   return new Promise(function(resolve, reject) {
-    auth().useDeviceLanguage()
+    auth().useDeviceLanguage();
     auth()
       .sendPasswordResetEmail(email)
-      .then(() => resolve('Email sent'))
-      .catch(err => reject(err))
-  })
-}
+      .then(() => resolve("Email sent"))
+      .catch(err => reject(err));
+  });
+};
 
 const resetPassword = function(actionCode, newPassword) {
   return new Promise(function(resolve, reject) {
-    auth().useDeviceLanguage()
+    auth().useDeviceLanguage();
     auth()
       .confirmPasswordReset(actionCode, newPassword)
-      .then(() => resolve('You can now sign in with your new password'))
-      .catch(err => reject(err))
-  })
-}
+      .then(() => resolve("You can now sign in with your new password"))
+      .catch(err => reject(err));
+  });
+};
 
 const signInWithGoogle = function(token_id) {
   return new Promise(function(resolve, reject) {
-    const credential = auth.GoogleAuthProvider.credential(token_id)
+    const credential = auth.GoogleAuthProvider.credential(token_id);
     auth()
       .signInWithCredential(credential)
       .then(({ user }) => resolve(user))
-      .catch(err => reject(err))
-  })
-}
+      .catch(err => reject(err));
+  });
+};
 
 module.exports = {
   createUserWithEmailAndPassword,
@@ -75,4 +92,4 @@ module.exports = {
   resetPassword,
   changePassword,
   signInWithGoogle
-}
+};
