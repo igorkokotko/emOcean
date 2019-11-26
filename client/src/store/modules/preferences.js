7ad465f1d5ac152eaac8dfdf8c8e39b47df935e2
Tetiana Reshetnik <reshetniktati@gmail.com>
2019-11-26 15:25:50 +0200

    fixed pref route

--- client/src/store/modules/preferences.js
+++ client/src/store/modules/preferences.js
@@ -1,72 +1,72 @@
 const state = {
   preferences: {
     'id_1': {
-      src: '/preferences/animals.jpg',
+      src: '../preferences/animals.jpg',
       title: 'animals',
       chosen: false
     },
     'id_2': {
-      src: '/preferences/cinema.jpg',
+      src: '../preferences/cinema.jpg',
       title: 'cinema',
       chosen: false
     },
     'id_3': {
-      src: '/preferences/culinary.jpg',
+      src: '../preferences/culinary.jpg',
       title: 'culinary',
       chosen: false
     },
     'id_4': {
-      src: '/preferences/dance.jpg',
+      src: '../preferences/dance.jpg',
       title: 'dance',
       chosen: false
     },
     'id_5': {
-      src: '/preferences/extreme.jpg',
+      src: '../preferences/extreme.jpg',
       title: 'extreme',
       chosen: false
     },
     'id_6': {
-      src: '/preferences/game.jpg',
+      src: '../preferences/game.jpg',
       title: 'game',
       chosen: false
     },
     'id_7': {
-      src: '/preferences/health.jpg',
+      src: '../preferences/health.jpg',
       title: 'health',
       chosen: false
     },
     'id_8': {
-      src: '/preferences/learn.jpg',
+      src: '../preferences/learn.jpg',
       title: 'learn',
       chosen: false
     },
     'id_9': {
-      src: '/preferences/nature.jpg',
+      src: '../preferences/nature.jpg',
       title: 'nature',
       chosen: false
     },
     'id_10': {
-      src: '/preferences/prank.jpg',
+      src: '../preferences/prank.jpg',
       title: 'prank',
       chosen: false
     },
     'id_11': {
-      src: '/preferences/science.jpg',
+      src: '../preferences/science.jpg',
       title: 'science',
       chosen: false
     },
     'id_12': {
-      src: '/preferences/sport.jpg',
+      src: '../preferences/sport.jpg',
       title: 'sport',
       chosen: false
     },
     'id_13': {
-      src: '/preferences/travel.jpg',
+      src: '../preferences/travel.jpg',
       title: 'travel',
       chosen: false
     },
     'id_14': {
-      src: '/preferences/fun.jpg',
+      src: '../preferences/fun.jpg',
       title: 'fun',
       chosen: false
     }
