fb8fc14bda5856c0189cda5cd510424673704d06
Tetiana Reshetnik <reshetniktati@gmail.com>
2019-12-02 18:40:18 +0200

    db request/store preferences

--- server/routes/addPreferences.js
+++ server/routes/addPreferences.js
@@ -4,25 +4,11 @@ const protected = require('../middleware/protectRoute')
 const {db} = require("../config/databaseConfig");
 
 
-const save = () => {
-    console.log('slkd')
-    db.collection('users').doc('DaZx0hMA6DUbZYWWJamasm3pXlN2').set({preferences: 'kuhkerqeru'})
-    .catch(err => {
-        console.log(err)
-
-    })
-    
-}
-
-
-// let save = function (req, res) {
-//     console.log(req.body[1])
-//     console.log(req.user)
-    // db.collection('users').doc('DaZx0hMA6DUbZYWWJamasm3pXlN2').add({preferences: 'kuhk'})   
-//   };
-
-
-
+let save = function (req, res) {
+    // console.log(req.body[1])
+    // console.log(req.user)
+    db.collection('users').doc(req.user).update({preferences: req.body})   
+  };
 
 
 router.post('/save', protected, save)
