16d978c31b8d6bba2c628d0346f138d0d03382f0
Khrystyna Korinetska <kornecka@rambler.ru>
2019-12-02 20:31:37 +0200

    created follow page component

---
+++ client/src/views/FollowList/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <q-tabs
+      v-model="tab"
+      align="justify"
+      active-color="primary"
+      indicator-color="primary"
+      narrow-indicator
+    >
+      <q-tab class="text-orange" name="followers" label="followers" />
+      <q-tab class="text-teal" name="following" label="following" />
+    </q-tabs>
+    <q-tab-panels
+      v-model="tab"
+      animated
+      transition-prev="scale"
+      transition-next="scale"
+    >
+      <q-tab-panel name="followers">
+        <followItem
+          :follow="followersGetter"
+        />
+        <p v-if="!followersGetter.length" class="notification">
+          no followers found...
+        </p>
+      </q-tab-panel>
+
+      <q-tab-panel name="following">
+        <followItem
+          :follow="followingGetter"
+        />
+        <p v-if="!followingGetter.length" class="notification">
+          no following found...
+        </p>
+      </q-tab-panel>
+    </q-tab-panels>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+import FollowItem from './FollowItem'
+export default {
+  methods: mapActions(['uploadProfile']),
+  computed: mapGetters(['followingGetter', 'followersGetter']),
+  mounted () {
+    this.uploadProfile()
+  },
+  data () {
+    return {
+      tab: this.$route.query.p
+    }
+  },
+  components: {
+    FollowItem
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .q-tab-panels {
+    margin: 10px auto;
+    @media only screen and (min-width: 1024px) {
+      max-width: 768px;
+      background-color: #fff;
+      box-shadow: 0 1px 5px rgba(0,0,0,0.2), 0 2px 2px rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12);
+      border-radius: 4px;
+    }
+    .q-tab-panel {
+      .notification {
+        font-size: 18px;
+        text-align: center;
+      }
+    }
+  }
+</style>
