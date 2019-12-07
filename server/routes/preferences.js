febcc86e4f0735194b506c7ee1db7a9e10952b3d
Tetiana Reshetnik <reshetniktati@gmail.com>
2019-12-07 22:46:57 +0200

    delete unnecassary comments

--- server/routes/preferences.js
+++ server/routes/preferences.js
@@ -2,22 +2,6 @@ const express = require('express')
 const router = express.Router()
 const protected = require('../middleware/protectRoute')
 const { save } = require('../controllers/preferences')
-// const {db} = require("../config/databaseConfig");
-
-
-// let save = function (req, res) {
-//   try {
-//     db.collection('users').doc(req.userId).update({preferences: req.body})    
-//       .then(resolve => {
-//         res.send('Code Updated')
-//       })
-//       .catch(error => {
-//         res.status(408).send('Error - ' + error)
-//       })
-//   } catch (error) {
-//     res.status(505).send('Error - ' + error)
-//   }
-// };
 
 router.post('/save', protected, save)
 
