884fe7235173d0e262b24955d08b1e0d15f3ad16
Tetiana Reshetnik <reshetniktati@gmail.com>
2019-12-07 23:46:33 +0200

    delete extra Qdialog

--- client/src/quasar.js
+++ client/src/quasar.js
@@ -88,13 +88,13 @@ Vue.use(Quasar, {
     QSelect,
     QSlider,
     QSeparator,
-    QToggle,
-    QBar,
+    QToggle,   
     QDialog,
     QBanner,
     QVirtualScroll,
     QScrollObserver,
-    QChatMessage
+    QChatMessage,
+    QBar
   },
   directives: {
     Ripple,
