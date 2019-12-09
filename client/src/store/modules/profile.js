db156e96a3875fcb11580e68188c4c91e94c0dc4
Khrystyna Korinetska <kornecka@rambler.ru>
2019-12-09 17:23:21 +0200

    added actions to upload profile, followers/followings, added mutations

--- client/src/store/modules/profile.js
+++ client/src/store/modules/profile.js
@@ -1,4 +1,5 @@
 import authService from '@/services/auth'
+import axios from 'axios'
 
 export default {
   namespaced: true,
@@ -18,6 +19,30 @@ export default {
     },
     updateMyProfileId (ctx, myProfileId) {
       return ctx.commit('updateMyProfileId', myProfileId)
+    },
+    uploadProfile ({ commit }, nickname) {
+      axios
+        .get('/api/profiles/get-profile?nickname=' + nickname)
+        .then(response => {
+          commit('updateProfile', response.data.profile)
+        })
+        .catch(error => console.log(error))
+    },
+    uploadFollowers ({ commit }, id) {
+      axios
+        .get('/api/profiles/get-followers/' + id)
+        .then(response => {
+          commit('updateFollowers', response.data)
+        })
+        .catch(error => console.log(error))
+    },
+    uploadFollowings ({ commit }, id) {
+      axios
+        .get('/api/profiles/get-followings/' + id)
+        .then(response => {
+          commit('updateFollowings', response.data)
+        })
+        .catch(error => console.log(error))
     }
   },
 
@@ -27,16 +52,27 @@ export default {
     },
     updateMyProfileId (state, myProfileId) {
       state.myProfileId = myProfileId
+    },
+    updateProfile (state, profileData) {
+      state.profile = profileData
+    },
+    updateFollowers (state, profileData) {
+      state.profile.followers = profileData
+    },
+    updateFollowings (state, profileData) {
+      state.profile.following = profileData
     }
   },
 
   state: {
     myProfile: {},
-    myProfileId: ''
+    myProfileId: '',
+    profile: {}
   },
 
   getters: {
     profileGetter (state) {
+      localStorage.lastProfileId = state.profile.profileId
       return state.profile
     },
     followingGetter (state) {
