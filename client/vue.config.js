ff7df332076e4ead0ba421f66f8d258ee06e51ba
Zahakailo <o.zahakailo@gmail.com>
2019-12-10 16:44:03 +0200

    Sass-variables added correct.

--- client/vue.config.js
+++ client/vue.config.js
@@ -19,7 +19,7 @@ module.exports = {
     loaderOptions: {
       sass: {
         prependData: `
-          @import "@/assets/styles/variables.scss";
+          @import "quasar/dist/quasar.sass";
         `
       }
     }
