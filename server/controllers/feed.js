f6023e11aaa0644fe0ecb510e81a63000b63d0a6
Tetiana Reshetnik <reshetniktati@gmail.com>
2019-12-10 22:43:32 +0200

    changed due to request

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -4,11 +4,14 @@ let getFeedAnonimus = function (req, res) {
     try {
         const collection = db.collection('posts').get()  
         collection.then((querySnapshot) => {
+            if (querySnapshot.empty) {
+                return res.status(400).send('No matching documents.')
+            }
             let posts = {}
             querySnapshot.docs.forEach((doc) => {
                 posts[doc.id] = doc.data();
             })
-            res.json({posts})
+            res.json(posts)
         })
     } catch (error) {
         res.status(500).send('Error - ' + error)
@@ -26,11 +29,14 @@ let getFeedByPreferences = function (req, res) {
                 const postsDocs = db.collection('posts')
                 const postsCollection = postsDocs.where('tag', 'array-contains-any', pref).get()
                 postsCollection.then((querySnapshot) => {
+                    if (querySnapshot.empty) {
+                        return res.status(400).send('No matching tags.')
+                    }
                     let posts = {}
                     querySnapshot.docs.forEach((doc) => {
                         posts[doc.id] = doc.data();
                     })
-                    res.json({posts})
+                    res.json(posts)
                 })
           })
     } catch (error) {
