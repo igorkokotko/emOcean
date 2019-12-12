4047d0dd4b83c304a7ffcc12748120e45b195156
Tetiana Reshetnik <reshetniktati@gmail.com>
2019-12-12 02:06:57 +0200

    delete route to deleted tabs

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -53,24 +53,12 @@ const routes = [
         component: () => import('../views/Settings/Tabs/EditPreferences/index.vue')
       },
       {
-        path: 'editFollowers',
-        component: () => import('../views/Settings/Tabs/EditFollowers/index.vue')
-      },
-      {
-        path: 'editLiked',
-        component: () => import('../views/Settings/Tabs/EditLiked/index.vue')
-      },
-      {
         path: 'changePassword',
         component: () => import('../views/Settings/Tabs/ChangePassword/index.vue')
       },
       {
         path: 'editPrivacy',
         component: () => import('../views/Settings/Tabs/EditPrivacy/index.vue')
-      },
-      {
-        path: 'editSaved',
-        component: () => import('../views/Settings/Tabs/EditSaved/index.vue')
       },
       {
         path: 'deleteAccount',
