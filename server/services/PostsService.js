eeaaa734c979345a775074f8cba92e791c0f5b07
nimchuk <nimchuk2011@gmail.com>
2019-12-17 03:30:02 +0200

    Add posts getters by views/tags/preferences/followings

--- server/services/PostsService.js
+++ server/services/PostsService.js
@@ -1,6 +1,6 @@
 const { db, storage } = require('../config/databaseConfig')
 
-const uploadVideo = function (video, userId, dest) {
+const uploadVideo = function(video, userId, dest) {
   const metaData = {
     contentType: video.mimetype
   }
@@ -10,6 +10,146 @@ const uploadVideo = function (video, userId, dest) {
   })
 }
 
+const getPostsByViews = function(paginateId, postsCount) {
+  const postsRefQuery = db
+    .collection('posts')
+    .orderBy('views', 'desc')
+    .limit(postsCount)
+
+  if (paginateId) {
+    const paginateRef = db.collection('posts').doc(paginateId)
+    return paginateRef.get().then(snapshot => {
+      const next = postsRefQuery.startAfter(snapshot)
+
+      return next.get().then(docSnapshots => {
+        if (docSnapshots.docs.length > 0) {
+          const lastIndex = docSnapshots.docs[docSnapshots.docs.length - 1].id
+          const data = []
+          docSnapshots.forEach(doc => {
+            data.push(doc.data())
+          })
+          return { data, lastIndex }
+        } else {
+          return 'No more posts left'
+        }
+      })
+    })
+  } else {
+    return postsRefQuery.get().then(docSnapshots => {
+      const lastIndex = docSnapshots.docs[docSnapshots.docs.length - 1].id
+      const data = []
+      docSnapshots.forEach(doc => {
+        data.push(doc.data())
+      })
+      return { data, lastIndex }
+    })
+  }
+}
+
+const getPostsByTags = function(tags, paginateId, postsCount) {
+  const postsRefQuery = db
+    .collection('posts')
+    .orderBy('createdAt', 'desc')
+    .where('tags', 'array-contains-any', tags)
+    .limit(postsCount)
+  if (paginateId) {
+    const paginateRef = db.collection('posts').doc(paginateId)
+    return paginateRef.get().then(snapshot => {
+      const next = postsRefQuery.startAfter(snapshot)
+      return next.get().then(docSnapshots => {
+        if (docSnapshots.docs.length > 0) {
+          const lastIndex = docSnapshots.docs[docSnapshots.docs.length - 1].id
+          const data = []
+          docSnapshots.forEach(doc => {
+            data.push(doc.data())
+          })
+          return { data, lastIndex }
+        } else {
+          return 'No more posts left'
+        }
+      })
+    })
+  } else {
+    return postsRefQuery.get().then(docSnapshots => {
+      if (docSnapshots.docs.length > 0) {
+        const lastIndex = docSnapshots.docs[docSnapshots.docs.length - 1].id
+        const data = []
+        docSnapshots.forEach(doc => {
+          data.push(doc.data())
+        })
+        return { data, lastIndex }
+      } else {
+        return 'No more posts left'
+      }
+    })
+  }
+}
+
+const getPostsByPreferences = function(id, paginateId, postsCount) {
+  const userProfileRef = db.collection('users1').doc(id)
+  const postsRefQuery = db
+    .collection('posts')
+    .orderBy('createdAt', 'desc')
+    .limit(postsCount)
+  return userProfileRef.get().then(userDoc => {
+    const { preferences } = userDoc.data()
+    if (paginateId) {
+      const paginateRef = db.collection('posts').doc(paginateId)
+      return paginateRef.get().then(snapshot => {
+        const next = postsRefQuery
+          .where('tags', 'array-contains-any', preferences)
+          .startAfter(snapshot)
+        return next.get().then(docSnapshots => {
+          if (docSnapshots.docs.length > 0) {
+            const lastIndex = docSnapshots.docs[docSnapshots.docs.length - 1].id
+            const data = []
+            docSnapshots.forEach(doc => {
+              data.push(doc.data())
+            })
+            return { data, lastIndex }
+          } else {
+            return 'No more posts left'
+          }
+        })
+      })
+    } else {
+      return postsRefQuery
+        .where('tags', 'array-contains-any', preferences)
+        .get()
+        .then(docSnapshots => {
+          if (docSnapshots.docs.length > 0) {
+            const lastIndex = docSnapshots.docs[docSnapshots.docs.length - 1].id
+            const data = []
+            docSnapshots.forEach(doc => {
+              data.push(doc.data())
+            })
+            return { data, lastIndex }
+          } else {
+            return 'No more posts left'
+          }
+        })
+    }
+  })
+}
+
+const getPostsByFollowings = function(id) {
+  const postsQueryRef = db
+    .collection('users-posts')
+    .where('followers', 'array-contains', id)
+  return postsQueryRef.get().then(snapshots => {
+    const posts = []
+    snapshots.forEach(snap => {
+      console.log(snap.data())
+      posts.concat(snap.data().posts)
+    })
+    return posts
+  })
+}
+
 module.exports = {
-  uploadVideo
+  uploadVideo,
+  getPostsByViews,
+  getPostsByFollowings,
+  getPostsByTags,
+  getPostsByPreferences
 }
