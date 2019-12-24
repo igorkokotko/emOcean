1ad0cbef3dacda1b4709413d60cc98a798dceb87
nimchuk <nimchuk2011@gmail.com>
2019-12-25 00:18:40 +0200

    Async/await style

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,30 +1,40 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
-const protected = require("../middleware/protectRoute")
-const mediaValidation = require("../middleware/mediaValidation")
+const protected = require('../middleware/protectRoute')
+const mediaValidation = require('../middleware/mediaValidation')
 const upload = require('../config/multerConfig')
 const {
   savePost,
   uploadVideos,
+  getPostsByViews,
+  getPostsByFollowings,
+  getPostsByPreferences,
+  getPostsByTags,
+  editPost,
+  deletePost,
+  getUserPosts,
   searchPosts
-} = require("../controllers/posts")
+} = require('../controllers/posts')
 
+router.post(
+  '/upload-videos',
+  protected,
+  upload.array('file', 2),
+  mediaValidation,
+  uploadVideos
+)
 router.post('/save-post', protected, savePost)
+router.delete('/delete-post/:postId', protected, deletePost)
+router.put('/edit-post/:postId', protected, editPost)
+
+router.get('/by-tags', getPostsByTags)
+router.get('/by-views', getPostsByViews)
+router.get('/by-followings', protected, getPostsByFollowings)
+router.get('/by-preferences', protected, getPostsByPreferences)
+router.get('/get-user-posts', getUserPosts)
+
 router.get('/search', searchPosts)
 
-router.post("/upload-videos", protected, upload.array("file", 2), mediaValidation, uploadVideos)
-
-// /api/posts/upload-videos?type=single-video
-// /api/posts/upload-videos?type=two-videos
-// /api/posts/upload-videos?type=video-and-audio
-
-// get popular posts (by likes) with pagination
-// get recommended posts from feed with pagination
-// get posts from my followers feed with pagination
-// edit my post
-// get post by id
-// get posts by userId
-// like/unlike  post
 
 
 module.exports = router;
