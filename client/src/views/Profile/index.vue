03f77b366df4b1d62bf5115746c96ae27dcba77d
Orest Kozolup <kozolup.yakiv@gmail.com>
2020-01-10 20:05:24 +0200

    corrected improperly written code, fixed redirection between profiles using beforeRouteUpdate, added banner YOU ARE BLOCKED

--- client/src/views/Profile/index.vue
+++ client/src/views/Profile/index.vue
@@ -1,21 +1,23 @@
 <template>
-  <div>
-    <div class="profile-header" :style="{ backgroundImage: userBackground }">
-      <div class="view-counter" v-if="profileGetter.counters">
-        <i class="fas fa-eye"></i>
-<!--        {{profileGetter.counters.views}}-->
-      </div>
-    </div>
+  <div v-if="!blocked">
+    <div class="profile-header" :style="{ backgroundImage: userBackground }"></div>
     <div class="row wrapp">
-      <div class="col-12 col-md-3 profile-card" >
-        <profile-card :profile="profileGetter"/>
+      <div class="col-12 col-md-3 profile-card">
+        <profile-card :profile="profileGetter" />
       </div>
-        <div class="col-12 col-md-9 profile-content">
-          <div class="col-12">
-            <switch-video></switch-video>
-          </div>
+      <div class="col-12 col-md-9 profile-content">
+        <div class="col-12">
+          <switch-video></switch-video>
         </div>
+      </div>
     </div>
+    <to-top-anchor></to-top-anchor>
+  </div>
+  <div v-else>
+    <q-banner class="bg-primary text-white banner">
+      <q-icon name="warning" class="text-white" style="font-size: 4rem;" />
+      <p class="block-banner-message">Unfortunately, you have been blocked by this user</p>
+    </q-banner>
   </div>
 </template>
 
@@ -23,14 +25,24 @@
 import { mapGetters, mapActions } from 'vuex'
 import ProfileCard from './Card/index'
 import switchVideo from './Card/switchVideo'
-
+import toTopAnchor from '../../components/ToTopAnchor'
 export default {
   components: {
     ProfileCard,
-    switchVideo
+    switchVideo,
+    toTopAnchor
+  },
+  data () {
+    return {
+      blocked: false
+    }
   },
   computed: {
-    ...mapGetters({ profileGetter: 'profile/profileGetter' }),
+    ...mapGetters({
+      profileGetter: 'profile/profileGetter',
+      userPosts: 'posts/userPostsGetter',
+      errorGetter: 'profile/errorGetter'
+    }),
     userBackground () {
       return this.profileGetter.user_background ? 'url(' + this.profileGetter.user_background + ')' : 'url(https://i.ytimg.com/vi/DiS7ZMwTA0I/maxresdefault.jpg)'
     },
@@ -38,18 +50,54 @@ export default {
       return this.$route.params.nickname
     }
   },
+  async created () {
+    await this.uploadProfile(this.nickname)
+    const blocks = this.errorGetter.filter(el => el.error === "You have been blocked by this user")
+    if (blocks.length !== 0) {
+      this.blocked = true
+      this.deleteError()
+    } else {
+      this.blocked = false
+    }
+    this.getUserPosts(this.profileGetter.userId)
+  },
   mounted () {
     this.uploadProfile(this.nickname)
   },
   methods: {
-    ...mapActions({ uploadProfile: 'profile/uploadProfile' })
+    ...mapActions({
+      uploadProfile: 'profile/uploadProfile',
+      getUserPosts: 'posts/getUserPostsAction',
+      deleteError: 'profile/deleteError'
+    })
+  },
+  beforeRouteUpdate (to, from, next) {
+    if (to.path.startsWith('/profile')) {
+      this.uploadProfile(to.params.nickname)
+    }
+    next()
+  },
+  watch: {
+    async nickname () {
+      await this.uploadProfile(this.nickname)
+      const blocks = this.errorGetter.filter(el => el.error === "You have been blocked by this user")
+      if (blocks.length !== 0) {
+        this.blocked = true
+        this.deleteError()
+      } else {
+        this.blocked = false
+      }
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
   .wrapp {
-    height: 500px;
+    min-height: calc(100vh - 260px);
+    .profile-content {
+      background: url("https://img5.goodfon.ru/wallpaper/nbig/1/ab/zima-sneg-snezhinki-fon-christmas-blue-winter-background-s-7.jpg");
+    }
   }
   .profile-header {
     background-position: center;
@@ -72,8 +120,5 @@ export default {
         color: blue;
       }
     }
-  }
-  .profile-content {
-    background-color: #9C27B0;
   }
 </style>
