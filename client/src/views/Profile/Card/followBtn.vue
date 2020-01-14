170390503ab56a8ac2b1dada2a382005473cf5cf
Khrystyna Korinetska <kornecka@rambler.ru>
2020-01-14 14:28:03 +0200

    updated follow-btn

--- client/src/views/Profile/Card/followBtn.vue
+++ client/src/views/Profile/Card/followBtn.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-     <button class="profile-card-button" :class="btnClasses" @click="changeFollowStatus">
-       {{ this.isFollowing ? 'Unfollow' : 'Follow' }}
-     </button>
+    <button
+      class="profile-card-button"
+      :class="btnClasses"
+      @click="changeFollowStatus"
+    >{{ isFollowing ? 'Unfollow' : 'Follow' }}</button>
   </div>
 </template>
 
@@ -14,13 +16,6 @@ export default {
       type: Boolean
     },
     id: String
-  },
-  methods: { ...mapActions({ uploadProfileAction: 'profile/uploadProfileAction' }),
-    changeFollowStatus () {
-      let action = this.isFollowing ? 'unfollow' : 'follow'
-      this.uploadProfileAction({ action: action, id: this.profileId })
-      this.isFollowing = !this.isFollowing
-    }
   },
   data () {
     return {
@@ -39,43 +34,59 @@ export default {
     }
   },
   watch: {
-    'following' (to) {
+    following (to) {
       this.isFollowing = to
+    }
+  },
+  methods: {
+    ...mapActions({
+      uploadProfileAction: 'profile/uploadProfileAction',
+      getMyProfile: 'profile/getMyProfile'
+    }),
+    async changeFollowStatus () {
+      let action = this.isFollowing ? 'unfollow' : 'follow'
+      await this.uploadProfileAction({ action: action, id: this.profileId })
+      this.isFollowing = !this.isFollowing
+      if (action === 'unfollow') {
+        this.$emit('update-count', -1)
+      } else {
+        this.$emit('update-count', 1)
+      }
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .profile-card-button {
-    min-width: 120px;
-    padding: 5px 30px;
-    cursor: pointer;
-    transition: all .3s;
-    border: none;
-    font-weight: 700;
-    font-size: 14px;
-    border-radius: 50px;
-    color: #FFF;
-    outline: none;
+.profile-card-button {
+  min-width: 120px;
+  padding: 5px 30px;
+  cursor: pointer;
+  transition: all 0.3s;
+  border: none;
+  font-weight: 700;
+  font-size: 14px;
+  border-radius: 50px;
+  color: #fff;
+  outline: none;
 
-    &.button-orange {
-       background: linear-gradient(45deg, #D5135A, #F05924);
-       box-shadow: 0 4px 30px rgba(223, 45, 70, 0.35);
+  &.button-orange {
+    background: linear-gradient(45deg, #d5135a, #f05924);
+    box-shadow: 0 4px 30px rgba(223, 45, 70, 0.35);
 
-      &:hover {
-         box-shadow: 0 7px 30px rgba(223, 45, 70, 0.75);
-         transform: translateY(-5px);
-       }
-      }
-    &.button-blue {
-      background: linear-gradient(45deg, lightseagreen, blue);
-      box-shadow: 0 4px 30px lightblue;
-
-      &:hover {
-        box-shadow: 0 7px 30px cornflowerblue;
-        transform: translateY(-5px);
-      }
+    &:hover {
+      box-shadow: 0 7px 30px rgba(223, 45, 70, 0.75);
+      transform: translateY(-5px);
     }
   }
+  &.button-blue {
+    background: linear-gradient(45deg, lightseagreen, blue);
+    box-shadow: 0 4px 30px lightblue;
+
+    &:hover {
+      box-shadow: 0 7px 30px cornflowerblue;
+      transform: translateY(-5px);
+    }
+  }
+}
 </style>
