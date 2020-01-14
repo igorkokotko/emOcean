5814d544256f0c4e2514af039585211b5f95fc2e
Khrystyna Korinetska <kornecka@rambler.ru>
2020-01-14 14:29:55 +0200

    updated profile card footer

--- client/src/views/Profile/Card/cardFooter.vue
+++ client/src/views/Profile/Card/cardFooter.vue
@@ -1,37 +1,44 @@
 <template>
   <div class="profile-footer">
     <nav class="stats row">
-      <router-link :to="{ name: 'followPage', params: { nickname: nickname }, query: { p: 'followers' }}">
-        <a href="#" class="stat">
-          <div class="stat-count">
-            {{ followers.length }}
-          </div>
-          <div class="stat-title">
-            Followers
-          </div>
-        </a>
+      <router-link
+        :to="{
+          name: 'followPage',
+          params: { nickname: nickname },
+          query: { p: 'followers' }
+        }"
+      >
+        <div class="stat">
+          <div class="stat-count">{{ followersCount }}</div>
+          <div class="stat-title">Followers</div>
+        </div>
       </router-link>
-        <a href="#" class="stat">
-          <div class="stat-count">
-           {{ 0 }}
-          </div>
-          <div class="stat-title">
-            Posts
-          </div>
-        </a>
-      <router-link :to="{ name: 'followPage', params: { nickname: nickname }, query: { p: 'following' }}">
-        <a href="#" class="stat">
-          <div class="stat-count">
-            {{ following.length }}
-          </div>
-          <div class="stat-title">
-            Following
-          </div>
-        </a>
+      <div class="stat">
+        <div class="stat-count">{{ userPosts.length || 0 }}</div>
+        <div class="stat-title">Posts</div>
+      </div>
+      <router-link
+        :to="{
+          name: 'followPage',
+          params: { nickname: nickname },
+          query: { p: 'following' }
+        }"
+      >
+        <div class="stat">
+          <div class="stat-count">{{ followingsCount }}</div>
+          <div class="stat-title">Following</div>
+        </div>
       </router-link>
     </nav>
     <div class="social-accounts">
-      <a v-for="account in socialAccounts" :title="`Come and find me in the ${account.type}`" :key="account.type" :href="account.link" class="social-icon" :class="`social-icon--${account.type}`">
+      <a
+        v-for="account in socialAccounts"
+        :title="`Come and find me in the ${account.type}`"
+        :key="account.type"
+        :href="account.link"
+        class="social-icon"
+        :class="`social-icon--${account.type}`"
+      >
         <i class="fab" :class="`fa-${account.type}`"></i>
       </a>
     </div>
@@ -39,23 +46,26 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   props: {
     socialAccounts: Array,
-    following: {
-      type: Array,
+    followingsCount: {
+      type: Number,
       default: function () {
-        return []
+        return 0
       }
     },
-    followers: {
-      type: Array,
+    followersCount: {
+      type: Number,
       default: function () {
-        return []
+        return 0
       }
     }
   },
   computed: {
+    ...mapGetters({ userPosts: 'posts/userPostsGetter' }),
     nickname () {
       return this.$route.params.nickname
     }
@@ -64,60 +74,60 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  $social-icon-colors: (
-    "facebook": #3b5999,
-    "youtube": #FF0000,
-    "instagram": #e4405f
-  );
-  .profile-footer > * {
-    margin-bottom: 15px;
-  }
-  .social-accounts {
+$social-icon-colors: (
+  "facebook": #3b5999,
+  "youtube": #ff0000,
+  "instagram": #e4405f
+);
+.profile-footer > * {
+  margin-bottom: 15px;
+}
+.social-accounts {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  .social-icon {
     display: flex;
     justify-content: center;
-    flex-wrap: wrap;
-    .social-icon {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      margin: 10px 15px;
-      border: 1px solid;
-      border-radius: 50%;
-      outline: none;
-      width: 40px;
-      height: 40px;
-      text-decoration: none;
-      background-color: white;
-      transition: 0.3s;
-      .fab {
-        font-size: 17px;
-      }
-      @each $name, $color in $social-icon-colors {
-        &--#{$name} {
-          color: $color;
+    align-items: center;
+    margin: 10px 15px;
+    border: 1px solid;
+    border-radius: 50%;
+    outline: none;
+    width: 40px;
+    height: 40px;
+    text-decoration: none;
+    background-color: white;
+    transition: 0.3s;
+    .fab {
+      font-size: 17px;
+    }
+    @each $name, $color in $social-icon-colors {
+      &--#{$name} {
+        color: $color;
         &:hover {
-           background-color: $color ;
-         }
+          background-color: $color;
+        }
         &:hover i {
-           color: white;
-         }
-       }
+          color: white;
+        }
       }
     }
   }
-  .stats {
-    display: flex;
-    justify-content: space-around;
-    & > * {
-      margin: 0 5px;
-    }
-    .stat-count {
-      font-weight: 600;
-      font-size: 16px;
-    }
-    .stat-title {
-      text-transform: uppercase;
-      font-size: 11px;
-    }
+}
+.stats {
+  display: flex;
+  justify-content: space-around;
+  & > * {
+    margin: 0 5px;
   }
+  .stat-count {
+    font-weight: 600;
+    font-size: 16px;
+  }
+  .stat-title {
+    text-transform: uppercase;
+    font-size: 11px;
+  }
+}
 </style>
