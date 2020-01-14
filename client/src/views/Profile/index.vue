f6f8a610999688c657cbb494c16d9d68cf2e34c1
Khrystyna Korinetska <kornecka@rambler.ru>
2020-01-14 14:37:37 +0200

    added action to upload videos, updated file

--- client/src/views/Profile/index.vue
+++ client/src/views/Profile/index.vue
@@ -1,17 +1,20 @@
 <template>
-  <div>
+  <div v-if="profileGetter.nickname">
     <div class="profile-header" :style="{ backgroundImage: userBackground }"></div>
     <div class="row wrapp">
-      <div class="col-12 col-md-3 profile-card" >
-        <profile-card :profile="profileGetter"/>
+      <div class="col-12 col-md-3 profile-card">
+        <profile-card :profile="profileGetter" />
       </div>
-        <div class="col-12 col-md-9 profile-content">
-          <div class="col-12">
-            <switch-video></switch-video>
-          </div>
+      <div class="col-12 col-md-9 profile-content">
+        <div class="col-12">
+          <switch-video :userPosts="userPosts" :userLikedPosts="userLikedPosts"></switch-video>
         </div>
+      </div>
     </div>
     <to-top-anchor></to-top-anchor>
+  </div>
+  <div v-else>
+    <error404></error404>
   </div>
 </template>
 
@@ -20,58 +23,76 @@ import { mapGetters, mapActions } from 'vuex'
 import ProfileCard from './Card/index'
 import switchVideo from './Card/switchVideo'
 import toTopAnchor from '../../components/ToTopAnchor'
+import Error404 from '../Error404/Error404'
 
 export default {
   components: {
     ProfileCard,
     switchVideo,
-    toTopAnchor
+    toTopAnchor,
+    Error404
   },
   computed: {
-    ...mapGetters({ profileGetter: 'profile/profileGetter' }),
+    ...mapGetters({
+      profileGetter: 'profile/profileGetter',
+      userPosts: 'posts/userPostsGetter',
+      userLikedPosts: 'posts/userLikedPostsGetter'
+    }),
     userBackground () {
-      return this.profileGetter.user_background ? 'url(' + this.profileGetter.user_background + ')' : 'url(https://i.ytimg.com/vi/DiS7ZMwTA0I/maxresdefault.jpg)'
+      return this.profileGetter.backgroundUrl
+        ? 'url(' + this.profileGetter.backgroundUrl + ')'
+        : 'url(https://i.ytimg.com/vi/DiS7ZMwTA0I/maxresdefault.jpg)'
     },
     nickname () {
       return this.$route.params.nickname
     }
   },
-  mounted () {
-    this.uploadProfile(this.nickname)
+  async beforeRouteUpdate (to, from, next) {
+    if (to.path.startsWith('/profile')) {
+      await this.uploadProfile(to.params.nickname)
+      this.getUserPosts(this.profileGetter.userId)
+      this.getUserLikedPosts(this.profileGetter.userId)
+    }
+    next()
+  },
+  async created () {
+    await this.uploadProfile(this.nickname)
+    this.getUserPosts(this.profileGetter.userId)
+    this.getUserLikedPosts(this.profileGetter.userId)
   },
   methods: {
-    ...mapActions({ uploadProfile: 'profile/uploadProfile' })
+    ...mapActions({
+      uploadProfile: 'profile/uploadProfile',
+      getUserPosts: 'posts/getUserPostsAction',
+      getUserLikedPosts: 'posts/getUserLikedPostsAction'
+    })
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .wrapp {
-    min-height: calc(100vh - 260px);
-    .profile-content {
-      background: url("https://img5.goodfon.ru/wallpaper/nbig/1/ab/zima-sneg-snezhinki-fon-christmas-blue-winter-background-s-7.jpg");
-    }
+.wrapp {
+  min-height: calc(100vh - 260px);
+  .profile-content {
+    background: url("https://img5.goodfon.ru/wallpaper/nbig/1/ab/zima-sneg-snezhinki-fon-christmas-blue-winter-background-s-7.jpg");
   }
-  .profile-header {
-    background-position: center;
-    background-size: cover;
-    height: 200px;
-    position: relative;
-    .view-counter {
-      position: absolute;
-      display: flex;
-      align-items: center;
-      font-size: 13px;
-      font-weight: 500;
-      top: 25px;
-      border-radius: 20px 0 0 20px;
-      right: 0;
-      padding: 5px;
-      background-color: white;
-      .fa-eye {
-        margin-right: 5px;
-        color: blue;
-      }
-    }
+}
+.profile-header {
+  background-position: center;
+  background-size: cover;
+  height: 200px;
+  position: relative;
+  .view-counter {
+    position: absolute;
+    display: flex;
+    align-items: center;
+    font-size: 13px;
+    font-weight: 500;
+    top: 25px;
+    border-radius: 20px 0 0 20px;
+    right: 0;
+    padding: 5px;
+    background-color: white;
   }
+}
 </style>
